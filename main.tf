ab229c89e85d369764973cafc6c2084c3b0c3764
nehrman <nicolas.ehrman@gmail.com>
2020-11-10 10:30:48 +0100

    Modifyong Module Version

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 module "rg" {
   source                     = "app.terraform.io/Hashicorp-neh-Demo/rg/azurerm"
-  version                    = "1.0.1"
+  version                    = "1.0.2"
   tf_az_name                 = "${var.az_name}"
   tf_az_env                  = "${var.az_env}"
   tf_az_location             = "${var.az_location}"
