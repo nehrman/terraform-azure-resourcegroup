b551dafc368123038b57bfa9ccdb32a88b889c6f
nehrman <nicolas.ehrman@gmail.com>
2021-01-27 09:33:40 +0100

    Update module version to 2.0.1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 module "rg" {
   source                     = "app.terraform.io/Hashicorp-neh-Demo/rg/azurerm"
-  version                    = "2.0.0"
+  version                    = "2.0.1"
   tf_az_name                 = var.az_name
   tf_az_env                  = var.az_env
   tf_az_location             = var.az_location
