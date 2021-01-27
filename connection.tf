ec9d562c819de5b50a64d34b4f40bc695326b97c
nehrman <nicolas.ehrman@gmail.com>
2021-01-27 09:19:56 +0100

    Upgrade to 0.12

--- connection.tf
+++ connection.tf
@@ -1,6 +1,7 @@
 # Define which provider to use with which credentials
 
 provider "azurerm" {
-    version = "=2.0.0"
-    features {}
+  features {
+  }
 }
+
--- main.tf
+++ main.tf
@@ -4,16 +4,14 @@
 module "rg" {
   source                     = "app.terraform.io/Hashicorp-neh-Demo/rg/azurerm"
   version                    = "1.0.2"
-  tf_az_name                 = "${var.az_name}"
-  tf_az_env                  = "${var.az_env}"
-  tf_az_location             = "${var.az_location}"
-  tf_az_net_addr_space       = "${var.az_net_addr_space}"
-  tf_az_subnet_prefixes      = "${var.az_subnet_prefixes}"
-  tf_az_subnet_names         = "${var.az_subnet_names }"
-  tf_az_storage_account_name = "${var.az_storage_account_name}"
+  tf_az_name                 = var.az_name
+  tf_az_env                  = var.az_env
+  tf_az_location             = var.az_location
+  tf_az_net_addr_space       = var.az_net_addr_space
+  tf_az_subnet_prefixes      = var.az_subnet_prefixes
+  tf_az_subnet_names         = var.az_subnet_names
+  tf_az_storage_account_name = var.az_storage_account_name
 
-  tf_az_tags = "${var.az_tags}"
+  tf_az_tags = var.az_tags
 }
 
-
-  
