f9e212a552a458b713661c81d10fea568ccc483c
nehrman <nicolas.ehrman@gmail.com>
2021-11-09 12:45:45 +0100

    Updating variables.tf

--- variables.tf
+++ variables.tf
@@ -17,13 +17,13 @@ variable "az_net_addr_space" {
 
 variable "az_subnet_prefixes" {
   description = "Define, via a list, the subnets prefixes. Must be in correlation with number or subnets"
-  type        = "list"
+  type        = list
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "az_subnet_names" {
   description = "Define, via a list, the name of subnets you wan to create"
-  type        = "list"
+  type        = list(string)
   default     = ["subnet1", "subnet2"]
 }
 
@@ -33,7 +33,7 @@ variable "az_storage_account_name" {
 
 variable "az_tags" {
   description = "Define the tags used for your ressources"
-  type        = "map"
+  type        = map(string)
   default = {
     "customer"    = "titi"
     "environment" = "dev"
